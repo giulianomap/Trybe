06f4ef23b182e1d5e8ad3473cbec01fc2acba87d
Giuliano Peixoto <giulianopeixoto@gmail.com>
2020-05-15 18:20:53 -0300

    modificação do exercício 4

--- exercises/4.4/4-ex-4.4.js
+++ exercises/4.4/4-ex-4.4.js
@@ -2,13 +2,14 @@ let info = {
   personagem: "Margarida",
   origem: "Pato Donald",
   nota: "Namorada do personagem principal nos quadrinhos do Pato Donald",
+  recorrente: "Sim"
 };
 
-info.recorrente = "Sim";
-
-for(let i in info) {
-  console.log(info[i]);
+for(let chave in info) {
+  console.log(info[chave]);
 }
+
+
 
 
 
