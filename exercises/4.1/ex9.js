c0f39e8fbd2f9ff528948f9ebfe5a15fce57dd00
Giuliano Peixoto <giulianopeixoto@gmail.com>
2020-05-17 11:58:42 -0300

    inclusão do exercício 9

---
+++ exercises/4.1/ex9.js
@@ -0,0 +1,10 @@
+let num1 = 4;
+let num2 = 5;
+let num3 = 10;
+let verificaParImpar = true;
+
+if(num1%2!==0 || num2%2!==0 || num3%2!==0) {
+  resultado = true
+} else resultado = false
+
+console.log(resultado);
