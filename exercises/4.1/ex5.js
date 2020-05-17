77f6aa02bd42eaa1b64a79de98eae72466e3b23e
Giuliano Peixoto <giulianopeixoto@gmail.com>
2020-05-17 10:26:17 -0300

    correção do exercício 5

--- exercises/4.1/ex5.js
+++ exercises/4.1/ex5.js
@@ -1,5 +1,11 @@
 let a = 60
-let b = 60;
+let b = 70;
 let c = 60;
+let somaAngulos = a+b+c;
 
 console.log(a+b+c === 180);
+
+if(somaAngulos !== 180) {
+  console.log("Erro: a soma dos ângulos deve ser 180º")
+}
+
