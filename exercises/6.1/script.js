59b04dd7848d76dde31389f3c9111140c8727bef
Giuliano Peixoto <giulianopeixoto@gmail.com>
2020-05-29 18:40:09 -0300

    criação de função para opções de estado e ajuste na estrutura do html

---
+++ exercises/6.1/script.js
@@ -0,0 +1,40 @@
+let statesList = [
+  ['Acre', 'AC'],
+  ['Alagoas', 'AL'],
+  ['Amapá', 'AP'],
+  ['Amazonas', 'AM'],
+  ['Bahia', 'BA'],
+  ['Ceará', 'CE'],
+  ['Distrito Federal', 'DF'],
+  ['Espírito Santo', 'ES'],
+  ['Goiás', 'GO'],
+  ['Maranhão', 'MA'],
+  ['Mato Grosso', 'MT'],
+  ['Mato Grosso do Sul', 'MS'],
+  ['Minas Gerais', 'MG'],
+  ['Pará', 'PA'],
+  ['Paraíba', 'PB'],
+  ['Paraná', 'PR'],
+  ['Pernambuco', 'PE'],
+  ['Piauí', 'PI'],
+  ['Rio de Janeiro', 'RJ'],
+  ['Rio Grande do Norte', 'RN'],
+  ['Rio Grande do Sul', 'RS'],
+  ['Rondônia', 'RO'],
+  ['Roraima', 'RR'],
+  ['Santa Catarina', 'SC'],
+  ['São Paulo', 'SP'],
+  ['Sergipe', 'SE'],
+  ['Tocantins', 'TO']
+];
+
+let select = document.querySelector('select');
+for(i = 0; i < statesList.length; i+=1) {
+  let options = document.createElement('option');
+  options.value = statesList[i][1];
+  options.innerHTML = statesList[i][0];
+  select.appendChild(options);
+}
+
+
+
